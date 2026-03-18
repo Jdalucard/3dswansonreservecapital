a1daf3bf5a65ad8efa62e090efe2ca381497a953
Jose Martinez <danieluz3188@gmail.com>
2026-03-18 08:21:38 -0500

    section2

--- src/components/Section2/Section2Mobile.tsx
+++ src/components/Section2/Section2Mobile.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useRef } from "react";
-import dynamic from "next/dynamic"; // 1. Importar dynamic
+import dynamic from "next/dynamic";
 
 const SwanModelViewer = dynamic(() => import("../Three/SwanModel"), {
   ssr: false,
@@ -59,32 +59,32 @@ export default function Section2Mobile() {
       ref={containerRef}
       className="relative w-full overflow-hidden bg-black py-20"
     >
-      <div className="pointer-events-none absolute inset-0 z-10 opacity-40">
+      <div className="pointer-events-none absolute inset-0 z-10 opacity-30">
         <svg
           width="100%"
           height="100%"
-          viewBox="0 0 400 800"
+          viewBox="0 0 400 1200"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
           preserveAspectRatio="none"
         >
           <motion.path
-            d="M 200 400 L 320 150"
+            d="M 350 0 L 200 550"
             stroke="white"
             strokeWidth="0.5"
             initial={{ pathLength: 0, opacity: 0 }}
             whileInView={{ pathLength: 1, opacity: 1 }}
-            transition={{ duration: 1.5, ease: "easeInOut" }}
+            transition={{ duration: 1.8, ease: "linear" }}
           />
           <motion.path
-            d="M 200 400 L 80 650"
+            d="M 200 550 L 50 1200"
             stroke="white"
             strokeWidth="0.5"
             initial={{ pathLength: 0, opacity: 0 }}
             whileInView={{ pathLength: 1, opacity: 1 }}
-            transition={{ duration: 1.5, ease: "easeInOut", delay: 0.2 }}
+            transition={{ duration: 1.8, ease: "linear", delay: 0.3 }}
           />
-          <circle cx="200" cy="400" r="2" fill="#dcc562" />
+          <circle cx="200" cy="550" r="2" fill="#dcc562" />
         </svg>
       </div>
 
@@ -102,15 +102,13 @@ export default function Section2Mobile() {
 
       <div className="relative z-20 flex flex-col items-center">
         <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, x: 20 }}
+          whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
-          className="relative mb-6 flex w-full flex-col items-end pr-8 text-right"
+          className="relative mb-6 flex w-full flex-col items-end pr-6 text-right"
         >
-          <div className="w-[75%]">
-            <ImageBoxMobile src={contentData[0].img} />
-          </div>
           <div className="w-[85%]">
+            <ImageBoxMobile src={contentData[0].img} />
             <TextBoxMobile
               text={contentData[0].text}
               number={contentData[0].number}
@@ -118,7 +116,7 @@ export default function Section2Mobile() {
           </div>
         </motion.div>
 
-        <div className="relative z-30 my-8 flex h-100 w-full items-center justify-center">
+        <div className="relative z-30 my-4 flex h-80 w-full items-center justify-center">
           <motion.div
             initial={{ opacity: 0, scale: 0.8 }}
             whileInView={{ opacity: 1, scale: 1 }}
@@ -126,23 +124,20 @@ export default function Section2Mobile() {
             viewport={{ once: true }}
             className="relative flex h-full w-full items-center justify-center"
           >
-            <div className="aspect-square h-full w-full max-w-87.5">
+            <div className="aspect-square h-full w-full max-w-70">
               <SwanModelViewer />
             </div>
           </motion.div>
         </div>
 
-        {/* Block 2 */}
         <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, x: -20 }}
+          whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
-          className="relative mt-6 flex w-full flex-col items-start pl-8 text-left"
+          className="relative mt-6 flex w-full flex-col items-start pl-6 text-left"
         >
-          <div className="w-[75%]">
-            <ImageBoxMobile src={contentData[1].img} />
-          </div>
           <div className="w-[85%]">
+            <ImageBoxMobile src={contentData[1].img} />
             <TextBoxMobile
               text={contentData[1].text}
               number={contentData[1].number}
@@ -151,8 +146,7 @@ export default function Section2Mobile() {
         </motion.div>
       </div>
 
-      {/* Center Glow */}
-      <div className="pointer-events-none absolute top-1/2 left-1/2 h-100 w-full -translate-x-1/2 -translate-y-1/2 bg-[#dcc562]/10 blur-[120px]" />
+      <div className="pointer-events-none absolute top-1/2 left-1/2 h-100 w-full -translate-x-1/2 -translate-y-1/2 bg-[#dcc562]/10 blur-[100px]" />
     </section>
   );
 }
